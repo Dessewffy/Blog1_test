fee84e92b8e19cf6b54a26a25480e4aab44f457d
Dessewffy <88779121+Dessewffy@users.noreply.github.com>
2024-02-17 17:03:01 +0100

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -5,6 +5,7 @@ from selenium.webdriver.chrome.options import Options
 def get_preconfigured_chrome_driver() -> webdriver.Chrome:
     options = Options()
     options.add_experimental_option('detach', True)
+    options.add_argument('--headless')
     # options.add_experimental_option('excludeSwitches', ['enable-logging'])
 
     return webdriver.Chrome(options=options)
